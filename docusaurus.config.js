785984e6bf20754dafa5fef5cc221dac4b03c670
洪志兵 <lkhzb@hotmail.com>
2020-05-08 15:54:40 +0800

    deplay

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,8 +3,8 @@ const versions = require('./versions.json');
 module.exports = {
   title: 'imgenius',
   tagline: '让每一个服务/运维商，都成为智慧的团队',
-  url: 'http://userguide.idongmobility.cn/',
-  baseUrl: '/',
+  url: 'https://idongmobility.github.io/',
+  baseUrl: '/imgenius-docs/',
   favicon: 'img/favicon.ico',
   organizationName: 'iDongMobility', // Usually your GitHub org/user name.
   projectName: 'imgenius-docs', // Usually your repo name.  
